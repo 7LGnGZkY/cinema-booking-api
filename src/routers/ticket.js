308d6a15880c937b4fd7775cdf467af3a1ac7731
7LGnGZkY <n35.nnlpd@slmail.me>
2022-06-21 09:54:45 +0100

    Commit

--- src/routers/ticket.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createTicket } = require('../controllers/ticket');
-
-const router = express.Router();
-
-router.post('/', createTicket);
-
-module.exports = router;
